7dceda045da49b249c7e668f609a21e5c97a3f7f
unknown <nucekm@gmail.com>
2020-04-25 03:37:12 +0700

    checkbox

--- app/src/main/java/com/example/drawableresource/MainActivity.java
+++ app/src/main/java/com/example/drawableresource/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.drawableresource;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+CheckBox mCbAndroid;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mCbAndroid = findViewById(R.id.checkbox);
+        mCbAndroid.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked == true){
+                    Toast.makeText(MainActivity.this,"Đã check",Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(MainActivity.this,"Bỏ check",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
