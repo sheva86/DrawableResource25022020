07fb33d40d13b886535e694fbae21ec77bd69de8
unknown <nucekm@gmail.com>
2020-04-25 03:58:26 +0700

    radiobutton radiogroup

--- app/src/main/java/com/example/drawableresource/MainActivity.java
+++ app/src/main/java/com/example/drawableresource/MainActivity.java
@@ -5,22 +5,41 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 CheckBox mCbAndroid;
+RadioGroup mRadioGroup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mCbAndroid = findViewById(R.id.checkbox);
-        mCbAndroid.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        mRadioGroup = findViewById(R.id.radioGroup);
+//        mCbAndroid = findViewById(R.id.checkbox);
+//        mCbAndroid.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+//                if (isChecked == true){
+//                    Toast.makeText(MainActivity.this,"Đã check",Toast.LENGTH_SHORT).show();
+//                }else {
+//                    Toast.makeText(MainActivity.this,"Bỏ check",Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//        });
+        mRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked == true){
-                    Toast.makeText(MainActivity.this,"Đã check",Toast.LENGTH_SHORT).show();
-                }else {
-                    Toast.makeText(MainActivity.this,"Bỏ check",Toast.LENGTH_SHORT).show();
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId){
+                    case R.id.radioAndroid :
+                        Toast.makeText(MainActivity.this, "Android", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.radioJava:
+                        Toast.makeText(MainActivity.this, "Java", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.radioPHP:
+                        Toast.makeText(MainActivity.this, "PHP", Toast.LENGTH_SHORT).show();
+                        break;
                 }
             }
         });
